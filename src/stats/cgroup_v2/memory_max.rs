c769dbab4529b091fd738d105d9b4bd3ca03307d
Ruksi Korpisara <me@ruk.si>
2025-03-03 15:41:08 +0200

    Read total memory max (i.e. limit) from cgroup v2

---
+++ src/stats/cgroup_v2/memory_max.rs
@@ -0,0 +1,73 @@
+use crate::stats::cgroup_v2::CgroupV2Provider;
+use std::io;
+
+/// Get total available memory from the cgroup v2 filesystem
+pub fn get_memory_max_kb<P: CgroupV2Provider>(provider: &P) -> io::Result<u64> {
+    let memory_max_text = provider.get_cgroup_v2_memory_max()?;
+
+    // In cgroup v2, `memory.max` value "max" means no limit
+    if memory_max_text.trim() == "max" {
+        // TODO: fallback to `procfs`
+        return Ok(0);
+    }
+
+    match memory_max_text.trim().parse::<u64>() {
+        Ok(mem_bytes) => Ok(mem_bytes / 1024),
+        Err(e) => Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("Invalid memory.max format: {}", e),
+        )),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::stats::cgroup_v2::MockCgroupV2Provider;
+
+    #[test]
+    fn test_get_memory_max_kb_normal() -> io::Result<()> {
+        let mut mock_provider = MockCgroupV2Provider::new();
+        mock_provider
+            .expect_get_cgroup_v2_memory_max()
+            .returning(|| Ok("1048576".to_string())); // 1MB in B
+
+        let memory_max_kb = get_memory_max_kb(&mock_provider)?;
+        assert_eq!(memory_max_kb, 1024); // 1MB in KB
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_memory_max_kb_unlimited() -> io::Result<()> {
+        let mut mock_provider = MockCgroupV2Provider::new();
+        mock_provider
+            .expect_get_cgroup_v2_memory_max()
+            .returning(|| Ok("max".to_string()));
+
+        let memory_max_kb = get_memory_max_kb(&mock_provider)?;
+        assert_eq!(memory_max_kb, 0); // TODO: should be an error?
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_memory_max_kb_invalid_format() {
+        let mut mock_provider = MockCgroupV2Provider::new();
+        mock_provider
+            .expect_get_cgroup_v2_memory_max()
+            .returning(|| Ok("invalid".to_string()));
+
+        let result = get_memory_max_kb(&mock_provider);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_get_memory_max_kb_io_error() {
+        let mut mock_provider = MockCgroupV2Provider::new();
+        mock_provider
+            .expect_get_cgroup_v2_memory_max()
+            .returning(|| Err(io::Error::new(io::ErrorKind::NotFound, "File not found")));
+
+        let result = get_memory_max_kb(&mock_provider);
+        assert!(result.is_err());
+    }
+}
--- src/stats/cgroup_v2/mod.rs
+++ src/stats/cgroup_v2/mod.rs
@@ -1,6 +1,7 @@
 use crate::stats::{utils, CpuUsageValue, ResourceType, SystemStatsSource};
 mod cpu_usage;
 mod memory_current;
+mod memory_max;
 mod num_cpus;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
@@ -44,7 +45,7 @@ impl<P: CgroupV2Provider> SystemStatsSource for CgroupV2Source<P> {
 
     fn get_memory_total_kb(&self) -> io::Result<u64> {
         debug!("Using cgroup v2 for memory total");
-        Err(io::Error::new(io::ErrorKind::Other, "Not implemented"))
+        memory_max::get_memory_max_kb(&self.provider)
     }
 
     fn is_available_for(&self, resource_type: &ResourceType) -> bool {
@@ -104,6 +105,15 @@ impl CgroupV2Provider for CgroupV2FilesystemReader {
         Ok(line)
     }
 
+    fn get_cgroup_v2_memory_max(&self) -> io::Result<String> {
+        let file = File::open(self.mem_max_path())?;
+        let mut reader = BufReader::new(file);
+        // `memory.max` file is just a single line...
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+        Ok(line)
+    }
+
     fn is_available_for(&self, resource_type: &ResourceType) -> bool {
         let path_to_check = match resource_type {
             ResourceType::NumCpus => self.cpu_max_path(),
@@ -120,5 +130,6 @@ pub trait CgroupV2Provider {
     fn get_cgroup_v2_cpu_stat(&self) -> io::Result<Vec<String>>;
     fn get_cgroup_v2_cpu_max(&self) -> io::Result<String>;
     fn get_cgroup_v2_memory_current(&self) -> io::Result<String>;
+    fn get_cgroup_v2_memory_max(&self) -> io::Result<String>;
     fn is_available_for(&self, resource_type: &ResourceType) -> bool;
 }
